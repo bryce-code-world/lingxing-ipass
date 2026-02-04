a0dcf2521ea03bb80ffb0f0634a78039abb1831c
beichen <fdsdkv110@qq.com>
2026-02-04 11:19:38 +0800

    测试

--- test/lingxing_boarding/config_test.go
+++ test/lingxing_boarding/config_test.go
@@ -15,17 +15,15 @@ func TestConfigService_GetPairListV2WithRawBody(t *testing.T) {
 	cli := newClient(t)
 
 	out, raw, err := cli.Config.GetPairListV2WithRawBody(context.Background(), lingxing.PairListV2Request{
-		Offset: 0,
+		Offset: 1,
 		Length: 20,
 		// 可按需指定店铺与平台过滤，避免全量查询。
 		PlatformCodes: []string{"10009"},
 		StoreIDs:      []string{"110658143132021760"},
 		// MSKU:           []string{"TEST3"},
-		SKU: []string{"FFDD00101YW2L"},
+		// SKU: []string{"FFDD00101YW2L"},
 		// StartTime:      "2024-01-01 00:00:00",
 		// EndTime:        "2024-01-31 23:59:59",
-		// UseCursor:      lingxing.PtrBool(true),
-		// CursorID:       lingxing.PtrInt64(1),
 	})
 	if err != nil {
 		t.Fatalf("GetPairListV2WithRawBody() err=%v", err)
